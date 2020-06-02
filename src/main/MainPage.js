351b0481fc3ca020847acca3e4726b7a5c867d37
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2020-06-02 09:11:51 +0800

    initial site deployment

--- src/main/MainPage.js
+++ src/main/MainPage.js
@@ -11,12 +11,14 @@ import MATCHES from "../graphql/queries/matches";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import TableRow from "@material-ui/core/TableRow";
+import Link from "@material-ui/core/Link";
+import Switch from "@material-ui/core/Switch";
 
 const useStyles = makeStyles((theme) => ({
     rootPaper: {
         display: 'flex',
         flexGrow: '1',
-        height: 'calc(100vh - 160px)',
+        height: 'calc(100vh - 100px)',
         elevation: 4,
         padding: theme.spacing(1.5),
         overflow: 'auto'
@@ -28,8 +30,13 @@ export default function MainPage() {
     const theme = useTheme()
     const classes = useStyles(theme)
 
+
+
     // Apollo: Get all matches
-    const {loading, error, data} = useQuery(MATCHES)
+    const {loading, error, data} = useQuery(MATCHES, {
+        fetchPolicy: 'network-only',
+        pollInterval: 500
+    })
 
     if (loading) {
         return (
@@ -52,48 +59,69 @@ export default function MainPage() {
     return (
         <Dashboard>
             <Grid container justify={'center'}>
-                <Grid item xs={6}>
+                <Grid item xs={12}>
                     <Paper className={classes.rootPaper} elevation={2}>
                         <Table>
                             <TableHead>
                                 <TableCell>
-                                    Candidate
+                                    <Typography variant="h6">
+                                        Candidate
+                                    </Typography>
                                 </TableCell>
                                 <TableCell>
-                                    Job Post
+                                    <Typography variant="h6">
+                                        Job Post
+                                    </Typography>
                                 </TableCell>
                                 <TableCell>
-                                    Match %
+                                    <Typography variant="h6">
+                                        Match %
+                                    </Typography>
                                 </TableCell>
                                 <TableCell>
-                                    Candidate Action
+                                    <Typography variant="h6">
+                                        Candidate Action
+                                    </Typography>
                                 </TableCell>
                                 <TableCell>
-                                    Employer Action
+                                    <Typography variant="h6">
+                                        Employer Action
+                                    </Typography>
                                 </TableCell>
                                 <TableCell>
-                                    Approved
+                                    <Typography variant="h6">
+                                        Approved
+                                    </Typography>
                                 </TableCell>
                             </TableHead>
                             {data.Match.map(match => (
                                 <TableRow>
                                     <TableCell>
-                                        {match.candidatePk}
+                                        <Link onClick={() => {}}>
+                                            {match.candidatePk}
+                                        </Link>
                                     </TableCell>
                                     <TableCell>
-                                        {match.jobPostPk}
+                                        <Link>
+                                            {match.jobPostPk}
+                                        </Link>
                                     </TableCell>
                                     <TableCell>
-                                        {match.matchPercentage}
+                                        <Link>
+                                            {match.matchPercentage.toFixed(2)}
+                                        </Link>
                                     </TableCell>
                                     <TableCell>
-                                        {match.CandidateMatchAction}
+                                        {match.CandidateMatchAction.name}
                                     </TableCell>
                                     <TableCell>
-                                        {match.EmployerMatchAction}
+                                        {match.EmployerMatchAction.name}
                                     </TableCell>
                                     <TableCell>
-                                        {match.approved}
+                                        <Switch
+                                            checked={match.approved}
+                                            onChange={(_, checked) => {}}
+                                        />
                                     </TableCell>
                                 </TableRow>
                             ))}
